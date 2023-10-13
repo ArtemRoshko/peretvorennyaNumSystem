ef65817612e0abb04744925b225e6deb23ec5718
ArtemRoshko <136159411+ArtemRoshko@users.noreply.github.com>
2023-10-13 15:40:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main()
+        {
+                string des = "";
+                int temp = 128;
+                int num;
+                bool started = false;
+
+                do
+                {
+                    Console.Write("Enter number = ");
+                    num = int.Parse(Console.ReadLine());
+                } while (num > 255 || num < 0);
+
+                while (temp >= 1)
+                {
+                    if (num >= temp)
+                    {
+                        des += "1";
+                        num -= temp;
+                        started = true;
+                    }
+                    else if (started)
+                    {
+                        des += "0";
+                    }
+                    temp /= 2;
+                }
+
+                Console.WriteLine(des);
+        }
+    }
+    
+}
